1e4e62752052db46fe2261197670dac9a38b5759
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-04-17 18:12:31 -0300

    chore: register controllers

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 import { StatusCodes } from 'http-status-codes'
+import { CitiesControllers } from '../controllers'
 
 
 const router = Router()
 
 router.get('/', (req, res) => {
-  return res.status(StatusCodes.OK).send('Primeira rota!')
+  return res.status(StatusCodes.OK).send('Olá, estou funcionando! ( * w * )')
 })
+
+router.get('/cidades', CitiesControllers.Create)
 
 
 export { router }
