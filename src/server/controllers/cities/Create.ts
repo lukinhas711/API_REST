d7a6f05217463047609c754c6ddee243f4f59963
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-04-19 14:17:27 -0300

    feat: handling all caught errors

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -4,10 +4,12 @@ import * as yup from 'yup'
 
 interface ICity {
   name: string,
+  state: string,
 }
 
 const bodyValidation: yup.Schema<ICity> = yup.object().shape({
-  name: yup.string().required().min(3)
+  name: yup.string().required().min(3),
+  state: yup.string().required().min(3)
 })
 
 export const Create = async (req: Request<{}, {}, ICity>, res: Response) => {
@@ -16,14 +18,19 @@ export const Create = async (req: Request<{}, {}, ICity>, res: Response) => {
   let validatedData: ICity | undefined = undefined
 
   try {
-    validatedData = await bodyValidation.validate(data)
-  } catch (error) {
-    const validatedError = error as yup.ValidationError
+    validatedData = await bodyValidation.validate(data, { abortEarly: false })
+  } catch (err) {
+    const yupError = err as yup.ValidationError
+    const errors: Record<string, string> = {}
+
+    yupError.inner.forEach(error => {
+      if (!error.path) return
+      errors[error.path] = error.message
+    })
+
     return res.status(StatusCodes.BAD_REQUEST).json({
-      errors: {
-        default: validatedError.message
-      }
+      errors
     })
   }
-  return res.status(StatusCodes.ACCEPTED).send(`Voce passou o valor ${data.name}`)
+  return res.status(StatusCodes.ACCEPTED).send(`A cidade de ${validatedData.name} no estado ${validatedData.state}`)
 }
