0fe410794bc04abee3edb508a0635ac97db7f822
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-04-19 13:44:15 -0300

    feat: validate error

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import * as yup from 'yup'
 
 interface ICity {
   name: string,
 }
 
-export const Create = (req: Request<{}, {}, ICity>, res: Response) => {
-  console.log(req.body.name)
+const bodyValidation: yup.Schema<ICity> = yup.object().shape({
+  name: yup.string().required().min(3)
+})
 
-  return res.send('Create!')
+export const Create = async (req: Request<{}, {}, ICity>, res: Response) => {
+
+  const data = req.body
+  let validatedData: ICity | undefined = undefined
+
+  try {
+    validatedData = await bodyValidation.validate(data)
+  } catch (error) {
+    const validatedError = error as yup.ValidationError
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: validatedError.message
+      }
+    })
+  }
+  return res.status(StatusCodes.ACCEPTED).send(`Voce passou o valor ${data.name}`)
 }
